a661909ca2099c4744b6ec6fc01ea37169df65b9
Mariela Belen Rascon <marielabrascon@gmail.com>
2022-05-26 02:34:06 -0300

    Changed email attribute and In/Out classes

--- app/models/users.py
+++ app/models/users.py
@@ -1,38 +1,20 @@
 #Pydantic
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field
 
 
 # models
 
-# LOOKUP pydantic EmailStr
 class UserBase(BaseModel):
-    nickname: str = Field(
-        min_length=1,
-        max_length=50,
-        example='Maru'
-    )
-    username: str = Field(
-        min_length=1,
-        max_length=50,
-        example='muwella'
-    )
-    email: str = Field(
-        regex=r"^[a-zA-Z0-9_.+-ñÑ]+@[a-zA-Z0-9-ñÑ]+\.[a-zA-Z0-9-.ñÑ]+$"
-    )
+    username: str = Field(min_length=1,max_length=50,example='muwella')
+    nickname: str = Field(min_length=1,max_length=50,example='Maru')
+    email: EmailStr
 
 
 # WIP hash password (security)
-class User(UserBase):
+class UserIn(UserBase):
     password: str
 
 
 class UserOut(UserBase):
     pass
 
-
-class UserInLogin():
-    pass
-
-
-class UserInCreate():
-    pass
