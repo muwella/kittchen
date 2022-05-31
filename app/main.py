7e042b1227ebfc3fb7ac090bfebb32d07f0ed876
Mariela Belen Rascon <marielabrascon@gmail.com>
2022-05-30 22:12:23 -0300

    Added a bunch of login/user things + more

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 #app
-from .models.users import UserIn, UserOut
+from .models.users import UserIn, UserInDB, UserOut
 from .models.ingredients import IngredientCategory, Ingredient
 from .models.recipes import RecipeCategory, Recipe
 #Python
@@ -8,7 +8,7 @@ from uuid import UUID
 from typing import Union
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie
 from fastapi import status
 
 # LOOKUP fastapi routerAPI (use @router instead of @app)
@@ -35,10 +35,42 @@ app = FastAPI()
 
 # HTTP methods
 
+### ROOT
+
 @app.get('/')
 def root():
     return {"message": "hello world"}
 
+
+
+### USER
+
+@app.post('/users/new')
+def create_user(user_in: UserIn):
+    # hash password
+    hashed_password = 'hashed' + user_in.password
+    # save user in DB
+        # .dict() unwraps the pydantic model UserIn and
+        # allows us to fill another model with the data
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+
+    return {'user in DB': user_in_db}
+
+
+
+### LOGIN
+
+@app.post('/login')
+def login(
+    username: str = Form(),
+    password: str = Form()
+):
+    # check for username in DB
+    # verify username and password match
+    return {'nickname': username}
+
+
+### RECIPES
 
 # TBD should i receive user_id to look for their recipes
     # or use a token? (is there a token in the GET header?)
@@ -67,9 +99,10 @@ def show_recipes(
     return response
 
 
-# NOTE cuando recibo un JSON de receta, si tiene la exacta
-    # estructura de mi modelo Recipe, se valida como Recipe?
-@app.post('/recipes/new')
+# DOUBT que pasa si no recibo algo que entre dentro del modelo Recipe?
+# WIP advanced user guide: return status code
+    # other from the default
+@app.post('/recipes/new', status_code=status.HTTP_201_CREATED)
 def create_recipe(recipe: Recipe):
     return {'all': 'received'}
 
@@ -98,6 +131,9 @@ def update_recipe(
     # edit recipe
     return {recipe_id: recipe}
 
+
+
+### INGREDIENTS
 
 # show all ingredients
 @app.get('/ingredients')
@@ -141,86 +177,3 @@ def update_ingredient(
     return {ingredient_id: Ingredient}
 
 
-
-## apuntes ##
-
-# DUDAS
-# unique? foreign key? passwords?
-# integrate to DB? how to CRUD DB?
-# deploy on heroku
-
-# HTTP:
-    # header
-    # body
-    # methods / "operations"
-        # POST - create
-        # GET - read
-        # PUT- update
-        # DELETE - delete
-
-        # OPTIONS
-        # HEAD
-        # PATCH
-        # TRACE
-
-# path/route/endpoint: lo que se agrega a la url del dominio
-
-# PATH PARAMETERS -> mandatory
-    # "path/something/{recipe_id}"
-# QUERY PARAMETERS -> optional
-    # "path/users/{user_id}/details?age=21&height=159"
-
-# REQUEST & RESPONSE BODY
-    # @app.post("/recipe/new")
-    # def create_recipe(recipe: Recipe = Body()):
-    #     return recipe
-
-# Si necesito algo obligatorio es PATH PARAMETER
-# No QUERY PARAMETER
-# Puede pasar que lo necesite en algun caso
-    # (un QUERY PARAMETER obligatorio)
-    # Pero es raro, aunque se puede
-
-# VALIDATIONS - Parámetros
-    # min_length
-    # max_length
-    # regex
-
-    # ge (greater or equal than) >=
-    # le (less or equal than) <=
-    # gt (greater than) >
-    # lt (less than) <
-
-    # SWAGGER
-        # title
-        # description
-
-# get.put: # el cliente tiene que enviar un REQUEST BODY a la API
-
-# config class is for API testing
-# class Config:
-#     schema_extra = {
-#         "example": {
-#             "name": "Noodles",
-#             "steps": "Boil and eat",
-#             "category": 'meal'
-#         }
-#     }
-
-# example parameter is also for API testing
-
-# return 2 dicts on 1 JSON (recipe_id & recipe)
-    # results = recipe_id.dict()
-    # results.update(recipe.dict())
-    # return results
-
-# HTTP Status code
-    # 100 Information
-    # 200 OK
-        # 201 Created
-        # 204 No content
-    # 300 Redirect
-    # 400 Client error
-        # 404 No exists
-        # 422 Validation error
-    # 500 Internal Server Error
