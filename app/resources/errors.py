5aede3837bc34a52d3b5e146328fc3bd2f05c86d
Mariela Belen Rascon <marielabrascon@gmail.com>
2022-06-03 22:35:18 -0300

    WIP API messages to errors

---
+++ app/resources/errors.py
@@ -0,0 +1,25 @@
+# NOTE prob not using ingredient texts in the future
+
+# API messages
+
+USER_DOES_NOT_EXIST_ERROR = "user does not exist"
+RECIPE_DOES_NOT_EXIST_ERROR = "recipe does not exist"
+INGREDIENT_DOES_NOT_EXIST_ERROR = "ingredient does not exist"
+INGREDIENT_ALREADY_EXISTS = "ingredient already exists"
+USER_IS_NOT_AUTHOR_OF_RECIPE = "you are not the author of this recipe"
+
+INCORRECT_LOGIN_INPUT = "incorrect email or password"
+USERNAME_TAKEN = "user with this username already exists"
+EMAIL_TAKEN = "user with this email already exists"
+
+UNABLE_TO_FOLLOW_YOURSELF = "you can't follow yourself"
+USER_IS_NOT_FOLLOWED = "you don't follow this user"
+USER_IS_ALREADY_FOLLOWED = "you follow this user already"
+
+WRONG_TOKEN_PREFIX = "unsupported authorization type"  # noqa: S105
+MALFORMED_PAYLOAD = "could not validate credentials"
+
+RECIPE_IS_ALREADY_FAVORITED = "you are already marked this recipe as favorite"
+RECIPE_IS_NOT_FAVORITED = "recipe is not favorited"
+
+AUTHENTICATION_REQUIRED = "authentication required"
