99577865726b3efd968d1ed1b9fb699ff9a29dcc
Mariela Belen Rascon <marielabrascon@gmail.com>
2022-06-08 20:33:50 -0300

    WIP database

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI
-from .routers import login, users, recipes, ingredients
-
-# LATER FatSecret Platform API -> will most probably use it
-
-# LOOKUP
-# user-agent, HTTP proxy, middleware
-
+# NOTE FatSecret Platform API -> will most probably use it
+# LOOKUP user-agent, HTTP proxy, middleware
 # FIXME use Schemas in routes instead of DB Models
 
-# WIP relationships
+from fastapi import FastAPI
+from .routers import users, recipes, login, ingredients
+
+from .database.database import SessionLocal, engine
+
+# NOTE find a better way to import this
+from .models.users import Base
+
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
-# app.include_router(login.router)
 app.include_router(users.router)
 app.include_router(recipes.router)
+# app.include_router(login.router)
 # app.include_router(ingredients.router)
