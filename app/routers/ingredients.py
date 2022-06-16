2fafbe7f0dcff9cc55e30178f394e8272289e2c7
Mariela Belen Rascon <marielabrascon@gmail.com>
2022-06-16 14:55:15 -0300

    Commented everything

--- app/routers/ingredients.py
+++ app/routers/ingredients.py
@@ -13,43 +13,43 @@ router = APIRouter(
 
 ### INGREDIENTS
 
-# show all ingredients
-@router.get('/ingredients')
-def show_ingredients(
-    name: Union[str, None] = Query(default=None),
-    category: Union[IngredientCategory, None] = Query(default=None)
-    ):
+# # show all ingredients
+# @router.get('/ingredients')
+# def show_ingredients(
+#     name: Union[str, None] = Query(default=None),
+#     category: Union[IngredientCategory, None] = Query(default=None)
+#     ):
 
-    # look for ingredients with query filters
-    return {name: category}
-
-
-@router.post('/ingredients/new')
-def create_ingredient(ingredient: Ingredient = Body()):
-    return {'new': 'ingredient'}
+#     # look for ingredients with query filters
+#     return {name: category}
 
 
-@router.get('/ingredients/{ingredient_id}')
-def show_ingredient(
-    ingredient_id: int = Path(
-        gt=0,
-        title='Ingredient',
-        description='Shows an ingredient'
-        )
-    ):
-    return {"ingredient_id": ingredient_id}
+# @router.post('/ingredients/new')
+# def create_ingredient(ingredient: Ingredient = Body()):
+#     return {'new': 'ingredient'}
+
+
+# @router.get('/ingredients/{ingredient_id}')
+# def show_ingredient(
+#     ingredient_id: int = Path(
+#         gt=0,
+#         title='Ingredient',
+#         description='Shows an ingredient'
+#         )
+#     ):
+#     return {"ingredient_id": ingredient_id}
 
 
 # TBD only if it's a custom ingredient?
 # NOTE if we use FatSecret API maybe we won't need to customize ingredients
-@router.put('/ingredients/{ingredient_id}/edit')
-def update_ingredient(
-    ingredient_id: int = Path(
-        title='Ingredient',
-        description='Updates an ingredient',
-        gt=0
-        ),
-    ingredient: Ingredient = Body(),
-    ):
-    # edit ingredient
-    return {ingredient_id: Ingredient}
+# @router.put('/ingredients/{ingredient_id}/edit')
+# def update_ingredient(
+#     ingredient_id: int = Path(
+#         title='Ingredient',
+#         description='Updates an ingredient',
+#         gt=0
+#         ),
+#     ingredient: Ingredient = Body(),
+#     ):
+#     # edit ingredient
+#     return {ingredient_id: Ingredient}
